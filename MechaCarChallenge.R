3ae67c33144d2e0dd10c5722d0d81daddc9a76b6
Jose_P <jspascual1111@gmail.com>
2021-09-10 17:26:13 -0400

    Real commit

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,11 +1,17 @@
+#Deliverable 1
 library(dplyr)
-MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-lm(qsec ~ hp,mtcars) #create linear model
-summary(lm(qsec~hp,mtcars)) #summarize linear model
-model <- lm(qsec ~ hp,mtcars) #create linear model
-yvals <- model$coefficients['hp']*mtcars$hp +
-model$coefficients['(Intercept)'] #determine y-axis values from linear model
-plt <- ggplot(mtcars,aes(x=hp,y=qsec)) #import dataset into ggplot2
-plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and linear model
-lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars) #generate multiple linear regression model
-summary(lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars)) #generate summary statistics
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(formula=mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) #create linear model
+summary(lm(formula=mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)) #summarize linear model
+
+#Deliverable 2
+SuspCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- SuspCoil %>% summarize(Mean_PSI = mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), StdDev_PSI = sd(PSI)) #create summary table
+lot_summary <- SuspCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI = mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), StdDev_PSI = sd(PSI), .groups = 'keep')
+head(SuspCoil)
+
+#Deliverable 3
+t.test(SuspCoil$PSI ,mu=1500)
+t.test(subset(SuspCoil,Manufacturing_Lot=="Lot1")$PSI ,mu=1500)
+t.test(subset(SuspCoil,Manufacturing_Lot=="Lot2")$PSI ,mu=1500)
+t.test(subset(SuspCoil,Manufacturing_Lot=="Lot3")$PSI ,mu=1500)
